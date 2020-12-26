26a70a70df70cf0b47dffff7bb1f09677f3db27d
HMessi <wdahmed25@yahoo.com>
2020-12-26 19:56:03 +0200

    Cloud Function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,14 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.checkAlias = functions.https.onCall((data, context) => {
+
+    const ref = admin.firestore().collection('users').doc(data.slug);
+    return ref.get().then(doc => {
+        return { unique: !doc.exists}
+    }).catch(err => {
+        throw new functions.https.HttpsError('faild to connect')
+    });
+
+})
